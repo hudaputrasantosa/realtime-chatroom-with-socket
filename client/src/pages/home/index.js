bc0de0aaf822eb6dc3027892384a37ce6ab23b81
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-05-14 20:22:31 +0700

    [create] client page and connect socket

---
+++ client/src/pages/home/index.js
@@ -0,0 +1,45 @@
+import styles from "./styles.module.css";
+import { useNavigate } from "react-router-dom";
+
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+  const navigate = useNavigate();
+  const joinRoom = () => {
+    if (room !== "" && username !== "") {
+      socket.emit("join_room", { username, room });
+    }
+  };
+
+  navigate("/chat", { replace: true });
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.formContainer}>
+        <h2>{`<Ruang Programmer/>`}</h2>
+        <input
+          className={styles.input}
+          placeholder="Username Kamu..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <select
+          className={styles.input}
+          onChange={(e) => setRoom(e.target.value)}>
+          <option>-- Select Room --</option>
+          <option value="javascript">JavaScript</option>
+          <option value="node">Node</option>
+          <option value="express">Express</option>
+          <option value="react">React</option>
+        </select>
+
+        <button
+          className="btn btn-secondary"
+          style={{ width: "100%" }}
+          onClick={joinRoom}>
+          Join Room
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
