9f9ac5a32059308c988a5d9ee8cd85bdc375facb
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-05-19 01:29:58 +0700

    [create] - client chatroom page

---
+++ client/src/pages/chat/messages.js
@@ -0,0 +1,47 @@
+import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
+
+const Messages = ({ socket }) => {
+  const [messagesRecieved, setMessagesReceived] = useState([]);
+
+  // Working when socket event receive from server
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      console.log(data);
+      setMessagesReceived((state) => [
+        ...state,
+        {
+          message: data.message,
+          username: data.username,
+          createdTime: data.createdTime,
+        },
+      ]);
+    });
+
+    return () => socket.off("receive_message");
+  }, [socket]);
+
+  function formatDateFromTimestamp(timestamp) {
+    const date = new Date(timestamp);
+    return date.toLocaleString();
+  }
+
+  return (
+    <div className={styles.messagesColumn}>
+      {messagesRecieved.map((msg, i) => (
+        <div className={styles.message} key={i}>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <span className={styles.msgMeta}>{msg.username}</span>
+            <span className={styles.msgMeta}>
+              {formatDateFromTimestamp(msg.createdtime)}
+            </span>
+          </div>
+          <p className={styles.msgText}>{msg.message}</p>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Messages;
